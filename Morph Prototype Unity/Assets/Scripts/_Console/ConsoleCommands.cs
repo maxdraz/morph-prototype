7e2b57dd4558c75e53fa2f9693732ff899ea71f6
maxdraz <maksdrazew@gmail.com>
2021-09-13 21:55:35 +0100

    customised console and working on custom commands

---
+++ Morph Prototype Unity/Assets/Scripts/_Console/ConsoleCommands.cs
@@ -0,0 +1,15 @@
+using QFSW.QC;
+using UnityEngine;
+
+public static class ConsoleCommands
+{
+    [Command("respawn")]
+    private static void Respawn()
+    {
+        var playerTrans = GameObject.FindWithTag("Player").transform;
+       playerTrans.position = new Vector3(0, 2, 0);
+       playerTrans.rotation = Quaternion.identity;
+       // to do
+       // static void 
+    }
+}
