096248601e78c1c73e898c4478c9e9431e8e58a1
Nick Stevenson <38294505+NicolasStevenson@users.noreply.github.com>
2021-09-18 17:58:08 +0100

    Creature Enum populated

--- Morph Prototype Unity/Assets/Scripts/Actors/ActorProperties/CreatureType.cs
+++ Morph Prototype Unity/Assets/Scripts/Actors/ActorProperties/CreatureType.cs
@@ -1,5 +1,24 @@
 public enum CreatureType
 {
     Mantis,
-    Deer
+    Deer,
+    Baboon,
+    Lion,
+    Snail,
+    Tortoise,
+    MonitorLizard,
+    Beetle,
+    Armadillo,
+    Bear,
+    Raccoon,
+    Frog,
+    Scorpion,
+    Weasel,
+    Warthog,
+    Rhino,
+    Crocodile,
+    Crab
+
+
+
 }
