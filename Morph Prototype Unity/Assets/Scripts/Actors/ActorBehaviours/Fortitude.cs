6c6b4e70e19abcc0ab2d3dca7b3ed631ba29a6c3
Nick Stevenson <38294505+NicolasStevenson@users.noreply.github.com>
2021-11-02 18:44:09 +0000

    Update Fortitude.cs

--- Morph Prototype Unity/Assets/Scripts/Actors/ActorBehaviours/Fortitude.cs
+++ Morph Prototype Unity/Assets/Scripts/Actors/ActorBehaviours/Fortitude.cs
@@ -22,18 +22,18 @@ public class Fortitude : MonoBehaviour
         currentFortitude = maxFortitude;
     }
 
-    public float ReduceFortitude(int fortDamage, string effect) 
+    public float ReduceFortitude(int fortDamage, string effect, float duration)
     {
         
         float lastFortitudeValue = currentFortitude;
         currentFortitude -= fortDamage;
-        DisableFortitudeRegen();
+        
 
-        if (currentFortitude <= 0) 
+        if (currentFortitude <= 0)
         {
-            
 
-            if (effect == "Stun") 
+
+            if (effect == "Stun")
             {
                 float secondaryStat = 0f;
                 //instead the dc is (fortDamage - (lastFortitudeValue * 3))
@@ -46,7 +46,7 @@ public class Fortitude : MonoBehaviour
 
             if (effect == "Root")
             {
-                float secondaryStat = 0f; 
+                float secondaryStat = 0f;
                 //instead the dc is (fortDamage - (lastFortitudeValue * 3))
             }
 
@@ -64,23 +64,78 @@ public class Fortitude : MonoBehaviour
             {
                 chanceTobeEffected = (fortDamage - (lastFortitudeValue * 3));
             }
-            else 
+            else
             {
                 chanceTobeEffected = (fortDamage - (lastFortitudeValue + secondaryStat));
             }
 
-
-            StatusCheck(chanceTobeEffected, effect);
-  
+            if (chanceTobeEffected > 0)
+            {
+                StatusCheck(chanceTobeEffected, effect, duration);
+            }
         }
 
-        Invoke("FortitudeRegen", fortitudeRegenDelay);
+        StartCoroutine("FortitudeRegen");
         return currentFortitude;
     }
 
-    void StatusCheck(float chance, string statusToApply) 
+    IEnumerator FortitudeRegen() 
     {
-        float randomNumber = Random.value;
+        
+        fortitudeRegenRate = 0f;
+
+        yield return new WaitForSeconds(fortitudeRegenDelay);
+
+        fortitudeRegenRate = 1f;
+    }
+
+    public void ImmediateCC(float DC, string effect, float duration)
+    {
+        if (effect == "stun")
+        {
+            float secondaryStat = 0f;
+            //instead the chanceToSuccumb is (DC - (maxFortitude * 1.5))
+        }
+
+        if (effect == "paralysis")
+        {
+            //float secondaryStat = agility
+        }
+
+        if (effect == "root")
+        {
+            float secondaryStat = 0f;
+            //instead the chanceToSuccumb is (DC - (maxFortitude * 1.5))
+        }
+
+        if (effect == "silence")
+        {
+            //float secondaryStat = intelligence
+        }
+
+        if (effect == "crippled")
+        {
+            //float secondaryStat = toughness
+        }
+
+        if (secondaryStat == 0)
+        {
+            chanceTobeEffected = (DC - (maxFortitude * 1.5f));
+        }
+        else
+        {
+            chanceTobeEffected = (DC - (maxFortitude + secondaryStat));
+        }
+
+        if (chanceTobeEffected > 0)
+        {
+            StatusCheck(chanceTobeEffected, effect, duration);
+        }
+    }
+
+    void StatusCheck(float chance, string statusToApply, float duration)
+    {
+        float randomNumber = (Random.value) * 100;
 
         if (randomNumber <= chanceTobeEffected)
         {
@@ -89,49 +144,116 @@ public class Fortitude : MonoBehaviour
 
         if (statusApplied)
         {
-            StatusEffect(statusToApply);
+            StatusEffect(statusToApply, duration);
+            Debug.Log(statusToApply + " had " + chance + " chance to happen, and succeeded with " + randomNumber);
         }
+
+        else
+        {
+            Debug.Log(statusToApply + " had " + chance + " chance to happen, and failed with " + randomNumber);
+        }
+
+        statusApplied = false;
     }
 
-    void FortitudeRegen() 
-    {
-        fortitudeRegenRate = 1f;
-    }
-
-    void DisableFortitudeRegen()
-    {
-        fortitudeRegenRate = 0f;
-    }
-
-    void StatusEffect(string statusToApply) 
+    void StatusEffect(string statusToApply, float duration)
     {
 
-        if (statusToApply == "Stun") 
+        if (statusToApply == "stun")
         {
-        
+            StartCoroutine("Stun", duration); 
         }
 
-        if (statusToApply == "Paralysis")
+        if (statusToApply == "paralysis")
         {
-
+            Paralysis(duration);
         }
 
-        if (statusToApply == "Root")
+        if (statusToApply == "root")
         {
-
+            Root(duration);
         }
 
-        if (statusToApply == "Silence")
+        if (statusToApply == "silence")
         {
-
+            Silence(duration);
         }
 
-        if (statusToApply == "Crippled")
+        if (statusToApply == "crippled")
         {
-            
+            Crippled(duration);
         }
 
     }
+
+    IEnumerator Stun(float duration) 
+    {
+        //Turn off movement and attack capabilities (weapon and special morphs)
+
+        yield return new WaitForSeconds(duration);
+
+        //Turn movement and attack capabilities back on
+
+        yield return null;
+    }
+
+    IEnumerator Blindness(float duration)
+    {
+        //Reduce perception and therefore accuracy stat to 0
+
+        yield return new WaitForSeconds(duration);
+
+        //Return perception to whatever value it should be at this moment
+
+        yield return null;
+    }
+
+    IEnumerator Paralysis(float duration)
+    {
+        //Reduce total move speed by 75%, reduce total attack speed by 50%, and disable all mobility
+
+        yield return new WaitForSeconds(duration);
+
+        //Return Agility and total attacks speeds to their respective values (Value + previously removed value), and enable mobility
+
+        yield return null;
+    }
+
+    IEnumerator Root(float duration)
+    {
+        //Turn off movement and remove mobility options
+
+        yield return new WaitForSeconds(duration);
+
+        //Turn movement back on and enable mobility options
+
+        yield return null;
+    }
+
+    IEnumerator Crippled(float duration)
+    {
+        //Similar to intimidation effect 'shaken' (- 20% damage dealt, -20% intimidation on defense, -20% energy gain from all sources)
+        //also slows target by 20-30% for the duration
+
+        yield return new WaitForSeconds(duration);
+
+        //remove the feects shown above
+
+        yield return null;
+    }
+
+    IEnumerator Silence(float duration)
+    {
+        //Disable all active special morphs (any active abilities on special morphs) 
+
+        yield return new WaitForSeconds(duration);
+
+        //Enable all active special morphs (any active abilities on special morphs) 
+
+        yield return null;
+    }
+
+
 
     // Update is called once per frame
     void Update()
@@ -146,5 +268,12 @@ public class Fortitude : MonoBehaviour
         {
             currentFortitude = currentFortitude + (10 * fortitudeRegenRate) * Time.deltaTime;
         }
+
+        //if (Input.GetKeyDown("left shift")) 
+        //{
+            //chanceTobeEffected = (Random.value) * 100;
+            //chanceTobeEffected = 30;
+            //StatusCheck(chanceTobeEffected, "stun");
+        //}
     }
 }
