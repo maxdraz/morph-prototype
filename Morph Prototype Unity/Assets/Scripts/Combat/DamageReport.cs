ab01ba217f18b0414f7cdf582521f99623ae0a61
Nick Stevenson <38294505+NicolasStevenson@users.noreply.github.com>
2021-12-01 19:13:47 +0000

    Damage Report mostly working and Brutal Spurs onhit Particles V1 finished

---
+++ Morph Prototype Unity/Assets/Scripts/Combat/DamageReport.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageReport : MonoBehaviour
+{
+    Color Ice = new Color(0, 1, 1, 1);
+    Color Fire = new Color(1, 0.3f, 0f, 1);
+    Color Electric = new Color(1, .92f, .16f, 1);
+    Color Poison = new Color(1, 0, 1, 1);
+    Color Acid = new Color(0, 1, 0, 1);
+
+    float randomForce;
+    public float damage;
+    public string damageType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomForce = Random.Range(2,-2);
+        Vector3 spawnDirection = new Vector3(randomForce , 5, 0);
+        GetComponent<Rigidbody>().AddForce(spawnDirection,ForceMode.Impulse);
+
+        GetComponent<TMPro.TextMeshPro>().text = damage.ToString();
+
+        if (damage > 75) 
+        {
+            float differential = (damage - 75) / 10;
+            int extraFontSize = (int)differential;
+            GetComponent<TMPro.TextMeshPro>().fontSize += extraFontSize;
+        }
+
+        if (damageType == "Ice") 
+        {
+            GetComponent<TMPro.TextMeshPro>().color = Ice;
+        }
+
+        if (damageType == "Fire")
+        {
+            GetComponent<TMPro.TextMeshPro>().color = Fire;
+        }
+
+        if (damageType == "Electric")
+        {
+            GetComponent<TMPro.TextMeshPro>().color = Electric;
+        }
+
+        if (damageType == "Poison")
+        {
+            GetComponent<TMPro.TextMeshPro>().color = Poison;
+        }
+
+        if (damageType == "Acid")
+        {
+            GetComponent<TMPro.TextMeshPro>().color = Acid;
+        }
+
+        if (damageType == "Bludgeoning")
+        {
+            GetComponent<TMPro.TextMeshPro>().color = Color.grey;
+        }
+
+        if (damageType == "Piercing")
+        {
+            GetComponent<TMPro.TextMeshPro>().color = Color.black;
+            GetComponent<TMPro.TextMeshPro>().outlineColor = Color.white;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
